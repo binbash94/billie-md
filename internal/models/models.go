6fb86d489d9d79c2e1eadb6088fb076a56e43e36
kassem nassar <kassem@kassems-mbp.home>
2024-11-14 18:08:48 -0500

    modified the qualifier model for more clarity

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,16 +1,16 @@
 package models
 
 type Qualifiers struct {
-	aptDate             int64
-	patientAge          uint8
-	numberOfBodySystems uint8
-	aptDuration         uint8
-	practiceType        string
-	isAptVirtual        string
-	attendeesPresent    string
-	primaryIssue        string
-	secondaryIssue      string
-	isRecentDischarge   string
-	formsCompleted      string
-	procedureType       string
+	AppointmentDate         int64  // Timestamp of the appointment
+	PatientAge              uint8  // Age of the patient
+	NumberOfBodySystems     uint8  // Number of body systems evaluated
+	AppointmentDuration_min uint8  // Duration of the appointment in minutes
+	PracticeType            string // Type of medical practice (e.g., FHO, Clinic, ER)
+	AppointmentType         string // Type of appointment (e.g., follow-up, consultation)
+	AttendeesPresent        string // List of attendees present at the appointment
+	PrimaryIssue            string // The primary medical issue being addressed
+	SecondaryIssue          string // Any secondary medical issues being addressed
+	IsRecentDischarge       bool   // Whether the patient was recently discharged from the hospital
+	FormsCompleted          string // Whether forms were completed before or during the appointment
+	ProcedureType           string // Type of procedure performed (if applicable)
 }
